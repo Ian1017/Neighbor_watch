f7c3b22c007a8a60179ddb40dd7ad406e99383ff
ian1017 <mdawidamengo@gmail.com>
2020-07-15 10:25:25 +0300

    Add Admin and User class

--- hood/settings.py
+++ hood/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'neighbour'
+    'neighbour',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
