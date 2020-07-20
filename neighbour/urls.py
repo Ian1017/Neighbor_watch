0a1e3a243b00d26cb308c4d79aa79b132a27f46d
ian1017 <mdawidamengo@gmail.com>
2020-07-20 23:07:43 +0300

    Create profile view and url

--- neighbour/urls.py
+++ neighbour/urls.py
@@ -5,6 +5,9 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     url('^$', views.welcome, name = 'welcome'),
+    url(r'^profile/', views.profile, name='profile'),
+    url(r'^register/', views.register, name='register'),
+    url(r'^create_profile/', views.create_profile, name='create_profile'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- neighbour/views.py
+++ neighbour/views.py
@@ -2,6 +2,34 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def welcome(request):
-    return render(request, 'index.html')
+
+
+@login_required(login_url='/accounts/login/')
+def index(request):
+    title = "Neighbourhood"
     
+    business = Business.objects.all()
+    context = {
+        "title": title,
+        "business": business
+    }
+    return render(request, 'index.html', context)
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/login')
+    else:
+        form = RegisterForm()
+    return render(request, 'registration/registration_form.html', {"form": form}))
+    
+@login_required(login_url='/login')
+def profile(request):
+    user = request.user
+    context = {
+        "user": user,
+    }
+    return render(request, 'profile.html', context)
+
