41a15c6698122b5db1e0212e093d5635e3131fce
ian1017 <mdawidamengo@gmail.com>
2020-07-20 20:48:08 +0300

    Create Hood modeland delete model Author

--- neighbour/models.py
+++ neighbour/models.py
@@ -22,3 +22,25 @@ class User(models.Model):
 
     def save_user(self):
         self.save()
+
+
+# class Author(models.Model):
+#     first_name = models.CharField(max_length =30)
+#     last_name = models.CharField(max_length =30)
+#     email = models.EmailField()
+
+#     def __str__(self):
+#         return self.author
+
+#     def save_author(self):
+#         self.save()
+
+class Hood(models.Model):
+    name = models.CharField(max_length=20)
+    location = models.CharField(max_length=20)
+    image = models.ImageField(upload_to='hood_photo', blank=True, default='hood_photo/hood.jpg')
+    pub_date = models.DateTimeField(auto_now_add=True)
+    admin = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name}'
