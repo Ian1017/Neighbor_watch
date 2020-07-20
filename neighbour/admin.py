0acc3048745ab354ce301f86cf6fc5cbdf69c202
ian1017 <mdawidamengo@gmail.com>
2020-07-20 22:42:12 +0300

    Create model Business and register it

--- neighbour/admin.py
+++ neighbour/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Admin, User, Hood, Profile
+from .models import Admin, User, Hood, Profile, Business
 
 # Register your models here.
 admin.site.register(Admin)
 admin.site.register(User)
 admin.site.register(Hood)
 admin.site.register(Profile)
+admin.site.register(Business)
